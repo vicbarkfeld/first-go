7f97b2f91943eb0f128071c88875b0955e82c17a
Victoria Barkfeld <82127044+vicbarkfeld@users.noreply.github.com>
2022-04-16 18:51:31 -0300

    Meu primeiro programa em Go
    Programa de monitoramento de sites 
    
    Projeto realizado pelo curso da Alura, Go: Iniciando com linguagem do google.

---
+++ hello/hello.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+import "os"
+import "net/http"
+
+func main() {
+
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+	
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa!")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço esse comando :/")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao(){
+	nome := "Victoria"
+    idade := 25
+    versao := 1.1
+    fmt.Println("Olá", nome, "sua idade é", idade)
+    fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu(){
+	fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido é:", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
+}
